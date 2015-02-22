dca8b40b7698fb24e564a17ed487e15c5cec85b1
Hubert Ngu <hubertngu@Huberts-MacBook-Pro.local>
2015-02-21 16:56:16 -0800

    we gawt dat l0g0 weet weet and youtube video

--- js/ampfit.js
+++ js/ampfit.js
@@ -322,3 +322,14 @@ function test(type, element) {
 	$(element).find('img').show();
 	$(element).find('.rate').css('margin-right', 0);
 }
+
+function setupOnPanelHover() {
+	// set up hover panels
+	// although this can be done without JavaScript, we've attached these events
+	// because it causes the hover to be triggered when the element is tapped on a touch device
+	$('.hover').hover(function(){
+		$(this).addClass('flip');
+	},function(){
+		$(this).removeClass('flip');
+	});
+}
