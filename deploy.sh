84eeb4485e834d0870d1d35fca8754d93a750d71
Hubert Ngu <hubertngu@master-VirtualBox.(none)>
2015-04-03 17:57:47 -0700

    stylin that gcalendar

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 while :
 do 
-	git ls-files | sudo xargs -i cp {} /var/lib/tomcat7/webapps/ampfit/{}
+	#git ls-files | sudo xargs -i cp {} /var/lib/tomcat7/webapps/ampfit/{}
+	sudo cp -r * /var/lib/tomcat7/webapps/ampfit/ && sudo chmod 777 -R /var/lib/tomcat7/webapps/ampfit/
 	sleep 1
 done
