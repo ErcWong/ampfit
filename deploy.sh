dc340a1927bff2aedad97a6ff757361ddcf72e0a
Hubert Ngu <hubertngu@Huberts-MacBook-Pro.local>
2015-07-19 15:46:59 -0700

    #48 #52

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 while :
 do 
 	#git ls-files | sudo xargs -i cp {} /var/lib/tomcat7/webapps/ampfit/{}
-	sudo cp -r * /var/lib/tomcat7/webapps/ampfit/ && sudo chmod 777 -R /var/lib/tomcat7/webapps/ampfit/
+	#sudo cp -r * /var/lib/tomcat7/webapps/ampfit/ && sudo chmod 777 -R /var/lib/tomcat7/webapps/ampfit/
+	cp -r * /usr/local/Cellar/tomcat/8.0.21/libexec/webapps/ampfit/
 	sleep 1
 done
