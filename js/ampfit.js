7f9c92a2ffc8d9eb7f8c6d068c2966e102bc620f
hubertngu <hubertngu@gmail.com>
2015-08-09 16:30:44 -0700

    #66 Added group training and sweat saturday sections in dropdown

--- js/ampfit.js
+++ js/ampfit.js
@@ -99,6 +99,10 @@ function loadPongstgram() {
 	});
 }
 
+function loadTwitter() {
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+}
+
 function justifyTestimonialLayout() {
 	if($(window).width() > 759 ){
 	    $('#testimonial_modal').addClass('wrapper');
